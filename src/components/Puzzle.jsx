05b22bd293581d63eff705f8fcc8d55a8d52b3cc
Rajat Porwal <rajatporwal@protonmail.com>
2024-05-28 15:21:53 +0530

    Created a selection ring with a glow

--- src/components/Puzzle.jsx
+++ src/components/Puzzle.jsx
@@ -2,18 +2,24 @@ import pic from '../assets/pic1.jpg';
 import { useState, useEffect, useRef } from 'react';
 
 const CharacterSelectionModal = (props) => {
+  // Controlls modal visibility
   const hide = props.show_modal ? "block" : "none";
   const closeModal = props.closeModal;
+  // Image Click Coordinates
   const imageX = props.image_loc[0];
   const imageY = props.image_loc[1];
+  // Page Click Coordinates
   const pageX = props.page_loc[0];
   const pageY = props.page_loc[1];
+  // Window Click Coordinates
   const windX = props.window_loc[0];
   const windY = props.window_loc[1];
+  // Used to get the height of Modal
   const elementRef = useRef(null);
+  // Used to switch modal up or down
   const [modalUp, setModalUp] = useState(0);
 
-  // Making Modal open upwards if close to bottom of window
+  // Modal open upwards if close to bottom of window
   useEffect(() => {
     const modalUpCondition = window.innerHeight - elementRef.current.clientHeight;
     if (windY > modalUpCondition) {
@@ -24,19 +30,24 @@ const CharacterSelectionModal = (props) => {
   }, [hide]);
 
   return (
-    <div className="character-selection-modal" id="imageModal" style={{display: hide, top: pageY - modalUp, left: pageX}} ref={elementRef}>
-      <span className="modal-x-button"  onClick={closeModal}>&times;</span>
-      <p>Image X: {imageX}</p>
-      <p>Image Y: {imageY}</p>
-      <p>Page X: {pageX}</p>
-      <p>Page Y: {pageY}</p>
-      <p>Wind X: {windX}</p>
-      <p>Wind Y: {windY}</p>
+    <div>
+      <div className="character-selection-radius" style={{display: hide, top: pageY - 17, left: pageX - 17}}>
+      </div>
+      <div className="character-selection-modal" id="imageModal" style={{display: hide, top: pageY - modalUp, left: pageX + 20}} ref={elementRef}>
+        <span className="modal-x-button" onClick={closeModal}>&times;</span>
+        <p>Image X: {imageX}</p>
+        <p>Image Y: {imageY}</p>
+        <p>Page X: {pageX}</p>
+        <p>Page Y: {pageY}</p>
+        <p>Wind X: {windX}</p>
+        <p>Wind Y: {windY}</p>
+      </div>
     </div>
   )
 };
 
 // Thanks to PaunescuDragos-99 / waldo-game-frontend
+// I didnt' have to knowledge to gather coordinates
 const Puzzle = () => {
   const [showModal, setShowModal] = useState(false);
 
@@ -47,10 +58,13 @@ const Puzzle = () => {
   const handleClick = (e) => {
     const elem = e.currentTarget;
     const { top, left } = elem.getBoundingClientRect();
+    // Image Click Coordinates
     const imageX = e.pageX - left - window.scrollX;
     const imageY = e.pageY - top - window.scrollY;
+    // Page Click Coordinates
     const pageX = e.pageX;
     const pageY = e.pageY;
+    // Window Click Coordinates
     const windX = e.clientX;
     const windY = e.clientY;
 
